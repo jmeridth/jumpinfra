5fbcf9551a74bea65ffd34d16a69224489502c3d
jmeridth <jmeridth@gmail.com>
2022-07-01 21:12:17 -0500

    all egress from lb

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -38,6 +38,14 @@ resource "aws_security_group" "lb" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+  egress {
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
     Name = "${var.stack_name}-sg-lb-${var.environment}"
   }
