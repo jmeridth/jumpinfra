3bf39c33d067e0981f5cfe3787a9e2ca64bdc169
jmeridth <jmeridth@gmail.com>
2022-08-11 08:31:50 -0500

    fix(cluster-logging) Remove ECS cluster logging
    Thought it was a good idea to have logging in container instances.
    
    It is not.  Especially if someone uses the `env` command and all secrets are listed in CloudWatch (i.e., database password)
    
    Can look at adding just cluster logging and not container logging later.
    
    Signed-off-by: jmeridth <jmeridth@gmail.com>

--- terraform/ecs_cluster.tf
+++ terraform/ecs_cluster.tf
@@ -4,22 +4,8 @@ resource "aws_ecs_cluster" "main" {
   configuration {
     execute_command_configuration {
       kms_key_id = aws_kms_key.ecs_cluster.arn
-      logging    = "OVERRIDE"
-
-      log_configuration {
-        cloud_watch_encryption_enabled = true
-        cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs_cluster.name
-      }
     }
   }
-  tags = {
-    Name = "${var.stack_name}-cluster-${var.environment}"
-  }
-}
-
-resource "aws_cloudwatch_log_group" "ecs_cluster" {
-  name       = "/ecs/${var.stack_name}-cluster-${var.environment}"
-  kms_key_id = aws_kms_key.ecs_cluster.arn
   tags = {
     Name = "${var.stack_name}-cluster-${var.environment}"
   }
